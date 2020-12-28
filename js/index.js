24318e2cf5ebec2c5701cd0a579a7a4fb5a23da3
Ronald Peraza <ronaldperaza460@gmail.com>
2020-12-28 10:23:38 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -13,16 +11,14 @@ const search = document.getElementById("search");
 const getMovies = async (url) => {
     const resp = await fetch(url);
     const respData = await resp.json();
-    
-    console.log(respData);
-    
+           
     showMovies(respData.results);
 }
 
 getMovies(APIURL);
 
 const showMovies = (movies) => {
-    // clear main
+    
     main.innerHTML = "";
 
     movies.forEach((movie) => {
